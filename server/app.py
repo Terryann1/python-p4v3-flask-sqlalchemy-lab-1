8237a7783c02b2f9829cea182ba68913187c13df
Terryann <malobaterryann@gmail.com>
2025-04-07 00:09:36 +0300

    sqlalchemy-lab

--- server/app.py
+++ server/app.py
@@ -21,6 +21,36 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def view(id):
+    
+    earthquake = db.session.get(Earthquake,id)
+    
+    if not earthquake:
+        return {"message": f"Earthquake {id} not found."}, 404
+    
+    
+    earthquake_dict = earthquake.to_dict()
+    
+   
+    return earthquake_dict, 200
+
+@app.route('/earthquakes/magnitude/<float:magnitude>') 
+def view_magnitude(magnitude):
+     
+    quakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    
+    quakes_list = [quake.to_dict() for quake in quakes]
+
+    
+    response = {
+        "count": len(quakes_list),
+        "quakes": quakes_list
+    }
+
+    return response, 200   
+
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -7,3 +7,20 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+class Earthquake(db.Model,SerializerMixin):
+    __tablename__="earthquakes"
+    id=db.Column(db.Integer,primary_key=True)
+    location=db.Column(db.String(),nullable=False)
+    magnitude=db.Column(db.Float,nullable=False)
+    year=db.Column(db.Integer,nullable=False)
+
+    def __repr__(self):
+        return f"<Earthquake {self.id} {self.magnitude} {self.location} {self.year}>"
+
+    def to_dict(self):
+        return {
+            "id": self.id,  
+            "magnitude": self.magnitude,
+            "location": self.location,
+            "year": self.year
+        }
